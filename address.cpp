52ae07269f1ee82d9460c8fea1ca8bf66fd16156
ClarenceChee <chisijie@hotmail.com>
2019-08-11 19:09:03 +0800

    learning

--- address.cpp
+++ address.cpp
@@ -25,6 +25,37 @@ int main()
 	*p_updates += 1;
 	cout << "Now updates = " << updates << endl;
 
+	int nights = 1001;
+	int * pt = new int;
+	*pt = 1001;
+
+	cout << "nights value = ";
+	cout << nights << ": location " << &nights << endl;
+	cout << "int ";
+	cout << "value = " << *pt << ": location = " << pt << endl;
+
+	double * pd = new double;
+	*pd = 10000001.0;
+
+	cout << "double "
+		 << "value = " << *pd << ": location =" << pd << endl;
+	cout << "location of pointer pd: " << &pd << endl;
+	cout << "size of pt = " << sizeof(pt)
+		 << ": size of *pt = " << sizeof(*pt) << endl;
+	cout << "size of pd = " << sizeof(pd);
+	cout << ": size of *pd = " << sizeof(*pd) << endl;
+
+	double * p3 = new double [3];
+	p3[0] = 0.2;
+	p3[1] = 0.5;
+	p3[2] = 0.8;
+	cout << "p3[1] is " << p3[1] << ".\n";
+	p3 += 1;
+	cout << "Now p3[0] is " << p3[0] << " and "
+		 << "p3[1] is " << p3[1] << ".\n";
+	p3 = p3 -1;
+	delete [] p3;
+
 	cin.get();
 	return 0;
 }
